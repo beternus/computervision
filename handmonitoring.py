a66836255ed736b890fb7976504a8865a73746d7
Bernardo <90293389+beternus@users.noreply.github.com>
2022-12-27 10:24:44 -0300

    Update handmonitoring.py

--- handmonitoring.py
+++ handmonitoring.py
@@ -12,5 +12,5 @@ while True:
 	print(results.multi_hand_landmarks)
 
 	if results.multi_hand_landmarks:
-	  for handLms in results.multi_hand_landmarks:
-mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)   //draw the hand and the connections
+		for handLms in results.multi_hand_landmarks:
+		mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)   //draw the hand and the connections
